3941332575be41115e2a1a29a9bb521de532e76f
migiel <igla.michal@gmail.com>
2016-09-15 22:30:57 +0200

    next commit

--- GitRepo/src/main/java/GitHubConnTest/MainClass.java
+++ GitRepo/src/main/java/GitHubConnTest/MainClass.java
@@ -4,5 +4,7 @@ public class MainClass {
 
     public static void main(String[] args) {
         System.out.println("Test Repo Git");
+        System.out.println("Making some little changes");
+
     }
 }
